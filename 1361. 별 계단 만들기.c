9cff4e53f90385096ede900fe5879d6128d32c71
KamiPer <gorjs3540@naver.com>
2018-09-10 13:48:22 +0900

    PM 1:49 / 월요일 / 수업 시간

---
+++ 1361. 별 계단 만들기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int num, i, j;
+	
+	scanf("%d", &num);
+	for(i=0; i<num; i++)
+	{   
+		for(j=0; j<i; j++)
+			printf(" ");
+		printf("**"); 
+		printf("\n");
+	}
+}
+
